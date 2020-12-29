d26277f71aed9a05b836a3888279e91627a5b3ab
Corentin Julienne <corentin.julienne92@gmail.com>
2020-12-28 20:25:48 -0600

    adding a functional devise and implementing cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
 
 puts "Cleaning coktails and ingredients in database for seeding purposes..."
 Cocktail.destroy_all
@@ -19,18 +20,25 @@ Ingredient.create(name: "Mint leaves")
 Ingredient.create(name: "Boubon")
 
 
-puts "Seeding new Cocktails for demo purposes (seeding)"
+puts "Seeding new Cocktails with photos for demo purposes (seeding)"
 
-Cocktail.create(name: "Mojito")
-Cocktail.create(name: "Cuba Libre")
-Cocktail.create(name: "Dry Martini")
-Cocktail.create(name: "Margarita")
-Cocktail.create(name: "Negroni")
-Cocktail.create(name: "Cosmopolitan")
-Cocktail.create(name: "Daiquiri")
-Cocktail.create(name: "Pina Coloda")
-Cocktail.create(name: "Gin Tonic")
-Cocktail.create(name: "Tequila Sunrise")
-Cocktail.create(name: "White Russian")
-Cocktail.create(name: "Old Fashioned")
+puts "creating cocktail 1 : Mojito"
+
+file = URI.open("https://images.unsplash.com/photo-1599021456807-25db0f974333?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=758&q=80")
+cocktail = Cocktail.create(name: "Mojito")
+cocktail.photo.attach(io: file, filename: 'mojito.jpeg', content_type: 'image.jpeg')
+cocktail.save!
+
+# Cocktail.create(name: "Mojito")
+# Cocktail.create(name: "Cuba Libre")
+# Cocktail.create(name: "Dry Martini")
+# Cocktail.create(name: "Margarita")
+# Cocktail.create(name: "Negroni")
+# Cocktail.create(name: "Cosmopolitan")
+# Cocktail.create(name: "Daiquiri")
+# Cocktail.create(name: "Pina Coloda")
+# Cocktail.create(name: "Gin Tonic")
+# Cocktail.create(name: "Tequila Sunrise")
+# Cocktail.create(name: "White Russian")
+# Cocktail.create(name: "Old Fashioned")
 
